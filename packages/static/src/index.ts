fad40095425ef463ce8d7ceb26e3b817567986f7
Shigma <shigma10826@gmail.com>
2025-03-19 17:00:33 +0800

    feat: support config.fallthrought and config.errorPages

--- packages/static/src/index.ts
+++ packages/static/src/index.ts
@@ -2,20 +2,25 @@ import { Context, z } from 'cordis'
 import fetchFile from '@cordisjs/fetch-file'
 import {} from '@cordisjs/plugin-server'
 import { pathToFileURL } from 'node:url'
-import { join } from 'node:path'
+import { join, resolve } from 'node:path'
+import { Dict } from 'cosmokit'
 
 export interface Config {
   path: string
   root: string
   download?: boolean
+  fallthrough?: boolean
   extensions: string[]
+  errorPages: Dict<string>
 }
 
 export const Config: z<Config> = z.object({
   path: z.string().required(),
   root: z.string().required(),
   download: z.boolean(),
-  extensions: z.array(String),
+  fallthrough: z.boolean(),
+  extensions: z.array(String).default(['.html', '.htm']),
+  errorPages: z.dict(String),
 })
 
 export const inject = {
@@ -32,13 +37,17 @@ export function apply(ctx: Context, config: Config) {
   }
 
   ctx.server.get(config.path + '{/*path}', async (req, res, next) => {
-    const filename = join(ctx.baseDir, config.root, req.url.slice(config.path.length))
+    const filename = join(resolve(ctx.baseDir, config.root), req.url.slice(config.path.length))
     let response = await _fetchFile(filename)
-    if (response.status === 200) return response
+    if (response.ok) return response
     for (const ext of config.extensions) {
       response = await _fetchFile(filename + ext)
-      if (response.status === 200) return
+      if (response.ok) return response
     }
-    return response
+    if (config.fallthrough) return next()
+    if (config.errorPages[response.status]) {
+      return _fetchFile(resolve(ctx.baseDir, config.root, config.errorPages[response.status]))
+    }
+    return new Response(null, { status: 404, statusText: 'Not Found' })
   })
 }

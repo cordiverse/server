83724ca93ddbaddb2fe1617e8a31a6dc2a0aa109
Shigma <shigma10826@gmail.com>
2025-03-19 16:45:23 +0800

    feat: support config.extensions

--- packages/static/src/index.ts
+++ packages/static/src/index.ts
@@ -8,12 +8,14 @@ export interface Config {
   path: string
   root: string
   download?: boolean
+  extensions: string[]
 }
 
 export const Config: z<Config> = z.object({
   path: z.string().required(),
   root: z.string().required(),
   download: z.boolean(),
+  extensions: z.array(String),
 })
 
 export const inject = {
@@ -22,10 +24,21 @@ export const inject = {
 }
 
 export function apply(ctx: Context, config: Config) {
-  ctx.server.get(config.path + '{/*path}', async (req, res, next) => {
-    return fetchFile(pathToFileURL(join(ctx.baseDir, config.root, req.url.slice(config.path.length))), {}, {
+  function _fetchFile(filename: string) {
+    return fetchFile(pathToFileURL(filename), {}, {
       download: config.download,
       onError: ctx.logger?.warn,
     })
+  }
+
+  ctx.server.get(config.path + '{/*path}', async (req, res, next) => {
+    const filename = join(ctx.baseDir, config.root, req.url.slice(config.path.length))
+    let response = await _fetchFile(filename)
+    if (response.status === 200) return response
+    for (const ext of config.extensions) {
+      response = await _fetchFile(filename + ext)
+      if (response.status === 200) return
+    }
+    return response
   })
 }
